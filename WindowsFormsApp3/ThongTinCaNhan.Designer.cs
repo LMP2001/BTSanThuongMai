cbbf865eac3aa32250f69f6b908aeb989c1c7cec
LMP2001 <lyminhp2k1@gmail.com>
2021-11-09 13:03:56 +0700

    add file

---
+++ WindowsFormsApp3/ThongTinCaNhan.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class ThongTinCaNhan
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // ThongTinCaNhan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(998, 587);
+            this.Name = "ThongTinCaNhan";
+            this.Text = "Thongtincanhan";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
